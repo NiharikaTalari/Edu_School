4ef60fa2ee7983f222aedfd9e994966d6d18a803
NiharikaTalari <niharikatalari123@gmail.com>
2025-09-02 13:07:29 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/School/", // <-- Add this line for GitHub Pages deployment
   server: {
     host: "::",
     port: 8080,
